04e8fec6ab629d9e0ca627be9bde9af225255158
Abid Rasheed <34671236+troop129@users.noreply.github.com>
2022-10-24 23:45:46 -0700

    Update Quizlet Editor.user.js
    Added some logic to extend box around answer, still hides buttons below the answer and does not work 100% of time.

--- Quizlet Editor.user.js
+++ Quizlet Editor.user.js
@@ -2,19 +2,19 @@
 // @name         Quizlet Editor
 // @match        http*://www.quizlet.com/explanations/textbook-solutions/*
 // @match        http*://quizlet.com/explanations/textbook-solutions/*
-// @version      0.1
+// @version      0.2
 // @description  Remove paywall for Quizlet answers
-// @author       troop129
+// @author       troop129 + josephyooo
 // @grant        none
 // @run-at  document-end
 // ==/UserScript==
-
 window.addEventListener('load', function() {
     'use strict';
     var paywall = document.getElementsByClassName('wugyavo');
     if(paywall.length != 0){
         paywall[0].remove();
         document.getElementsByClassName('ExplanationSolutionsContainer hnqbbas s1oluvjw')[0].style.overflow="visible";
+        let newHeight = document.getElementsByClassName('ExplanationsSolutionCard c5ngj6s')[0].offsetHeight;
+        document.getElementsByClassName('mv7e89c')[0].style.minHeight=newHeight+"px";
     }
-    // document.getElementsByClassName('SiteFooter')[0].remove();
 }, false);
